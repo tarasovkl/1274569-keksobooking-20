dfd9cfa8a9cf19fc4d424e7c1e80b43fc2b32837
tarasovkl <tarasovk1991@gmail.com>
2020-06-21 19:54:09 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,78 +1,70 @@
 "use strict"
 
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
 var mapPins = document.querySelector('.map__pins');
-
 var elem = document.createElement('div');
-
+var avatarPath = ["img/avatars/user01.png", "img/avatars/user02.png", "img/avatars/user03.png", "img/avatars/user04.png", "img/avatars/user05.png", "img/avatars/user06.png", "img/avatars/user07.png", "img/avatars/user08.png",];
+var offerType = ["palace", "flat", "house", "bungalo"];
+var offerCheckin = ["12:00", "13:00", "14:00"];
+var offerRooms = [1, 2, 3];
+var offerFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var offerPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 var arr = [];
+var template = document.querySelector('#pin').content;
+var templateButton = document.querySelector('#pin').content.querySelector('button');
+var templateImg = document.querySelector('#pin').content.querySelector('img');
 
-for (var i = 1; i < 9; i++) {
-  arr[i] = {
-    author: {
-      avatar: "img/avatars/user0" + i + ".png",
-    },
-    offer: {
-      title: "",
-      address: "",
-      price: 1000,
-      type: "",
-      rooms: 2,
-      guests: 2,
-      checkin: "",
-      checkout: "",
-      features: [],
-      description: "",
-      photos: [],
-    },
-    location: {
-      x: 100 + 100 * i,
-      y: 400
-    }
-  }
-  var makeElement = function (tagName, className) {
-    var element = document.createElement(tagName);
-    if (className) {
-    element.classList.add(className);
-    }
-    return element;
-  };
-
-  var createPin = function (map__pin) {
-    var newButton = makeElement('button', 'map__pin');
-    newButton.style = "left: " + map__pin.location.x + "px;" + "top: " + map__pin.location.y + "px;";
-    newButton.type = "button";
-    // newButton.setAttribute("type", "button")
-    elem.appendChild(newButton);
-
-    var picture = makeElement('img');
-    picture.src = map__pin.author.avatar;
-    picture.width = "40";
-    picture.height = "40";
-    picture.draggable = "";
-    picture.alt = "Метка объявления"
-    newButton.appendChild(picture);
-
-    return newButton;
-  }
-  var pinItem = createPin(arr[i]);
+function getRandom(arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
 }
 
-arr.shift();
+function getRandomNumber(min, max) {
+  var randNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randNumber);
+}
 
-var template = document.querySelector('#pin').content;
+map.classList.remove('map--faded');
 
-var templateButton = document.querySelector('#pin').content.querySelector('button');
+var createData = function (value) {
+  for (var i = 0; i < value; i++) {
+    arr[i] = {
+      author: {
+        avatar: avatarPath[i]
+      },
+      offer: {
+        title: "Заголовок",
+        address: "600, 350",
+        price: 1000,
+        type: getRandom(offerType),
+        rooms: getRandom(offerRooms),
+        guests: 1,
+        checkin: getRandom(offerCheckin),
+        checkout: getRandom(offerCheckin),
+        features: [offerFeatures.slice(0,getRandomNumber(1,offerFeatures.length))],
+        description: "Описание",
+        photos: [offerPhotos.slice(0,getRandomNumber(1,offerPhotos.length))],
+      },
+      location: {
+        x: getRandomNumber(100, 1000),
+        y: getRandomNumber(130, 600)
+      }
+    }
+  }
+};
 
-template.removeChild(templateButton);
-template.appendChild(pinItem);
-elem.append(pin.content.cloneNode(true));
-mapPins.appendChild(elem);
+createData(8);
 
-console.log(template);
-console.log(templateButton);
+var createButton = function (data) {
+  for (var i = 0; i < data.length; i++) {
+    templateImg.alt = data[i].offer.title;
+    templateImg.src = data[i].author.avatar;
+    var button = templateButton.cloneNode(true);
+    button.style = "left: " + data[i].location.x + "px;" + "top:" + data[i].location.y + "px;";
+    mapPins.appendChild(button);
+  }
+};
+createButton(arr);
 console.log(arr);
 
 
